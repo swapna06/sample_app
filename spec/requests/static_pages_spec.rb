039e333460906256b8ad52abf2bb4e506635f0f6
Swapna Challa <swappu06@gmail.com>
2014-01-03 01:05:02 -0500

    Finish of StaticPages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,11 +6,23 @@ describe "StaticPages" do
   		visit "/static_pages/home"
   		expect(page).to have_content("Sample App")
   	end
+
+  	it "should have title Home" do
+  		visit "/static_pages/home"
+  		expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+  	end
+
   end
+
   describe "Help Page" do 
   	it "should have content help" do
   		visit "static_pages/help"
   		expect(page).to have_content("Help")
+  	end
+
+  	it "should have title Help" do
+  		visit "/static_pages/help"
+  		expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
   	end
   end
 
@@ -19,6 +31,12 @@ describe "StaticPages" do
   		visit "static_pages/about"
   		expect(page).to have_content("About Us")
   	end
+
+  	it "should have title About" do
+  		visit "/static_pages/about"
+  		expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+  	end
+
   end
 
 end
